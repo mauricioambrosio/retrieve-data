989b26d296044cf6195e40a8ce86f0785fb4c8af
Mauricio Ambrosio <mauristaff@hotmail.com>
2021-03-13 12:38:36 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div 
+      v-if="stories.length>0"
+      class="container"
+    >
+      <h3>Top 10 stories</h3>
+      
+      <div 
+        class="card mb-4 border shadow"
+        v-for="story in stories" 
+        :key="story.id"
+      >
+        <div class="card-header">
+          <b>{{story.title}}</b>
+        </div>
+
+        <div class="card-body">
+          <p v-if="story.text" v-html="story.text"/>
+          <a v-else-if="story.url" :href="story.url">{{story.url}}</a>
+        
+        </div>
+        
+        <br/>
+      
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data:function(){
+    return {
+      storyIds: [],
+      stories:[]
+    }
+  },
+  methods:{
+    // fetch one item/story based on id
+    fetchStory(id){
+      return new Promise((resolve)=>{
+        fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`, {
+          method: "get"
+        }).then((response) => {
+          return response.json()
+        }).then((jsonData)=>{
+          resolve(jsonData)
+        })
+      })
+    
+    }
+  },
+  mounted:function(){
+    // fetch top story ids
+    fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty", {
+      method: "get"
+    }).then((response) => {
+      return response.json()
+    }).then(async (jsonData)=>{
+      // consider only the top 10
+      this.storyIds = jsonData.splice(0,10)
+      
+      // get each story based on id and add to stories
+      let i;
+      for(i=0; i<this.storyIds.length; i++){
+        const story = await this.fetchStory(this.storyIds[i]); 
+        this.stories.push(story)
+      }
+    })
+  },
 }
 </script>
 
